590aac650dff75f31da862a8265698b0fd9fc706
Azlan Ainto <azlanainto97@gmail.com>
2026-06-07 12:52:48 +0200

    Konveriterung von User Input in  float, integer. Verkettung

--- Tag4/TypeTest.py
+++ Tag4/TypeTest.py
@@ -1,2 +1,7 @@
 # --- Testing the type of the user input
 print(type(input("Wert: ")))
+r = input("R in Ohm:")
+print(r*2) # --> Fehler --> verkettet r 
+# --- Konvertiere r in float ----
+r = float(r)
+print(r*2)
